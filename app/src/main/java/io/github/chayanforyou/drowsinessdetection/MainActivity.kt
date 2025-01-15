2c071fb6f723e0d792643f04c91270c713f18561
Chayan Mistry <chayanmistrry@gmail.com>
2025-01-15 12:33:39 +0600

    initial commit

---
+++ app/src/main/java/io/github/chayanforyou/drowsinessdetection/MainActivity.kt
@@ -0,0 +1,122 @@
+package io.github.chayanforyou.drowsinessdetection
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
+import com.google.mlkit.common.MlKitException
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import io.github.chayanforyou.drowsinessdetection.overlay.GraphicOverlay
+import io.github.chayanforyou.drowsinessdetection.processors.FaceDetectorProcessor
+import io.github.chayanforyou.drowsinessdetection.processors.VisionImageProcessor
+import io.github.chayanforyou.drowsinessdetection.viewmodels.CameraXViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var previewView: PreviewView
+    private lateinit var graphicOverlay: GraphicOverlay
+    private var cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+    private var cameraProvider: ProcessCameraProvider? = null
+    private var analysisUseCase: ImageAnalysis? = null
+    private var imageProcessor: VisionImageProcessor? = null
+    private var needUpdateOverlayInfo = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        previewView = findViewById(R.id.preview_view)
+        graphicOverlay = findViewById(R.id.graphic_overlay)
+
+        ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        )[CameraXViewModel::class.java]
+            .processCameraProvider
+            .observe(this) { provider: ProcessCameraProvider? ->
+                cameraProvider = provider
+                bindAllCameraUseCases()
+            }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        bindAllCameraUseCases()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        imageProcessor?.run { this.stop() }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        imageProcessor?.run { this.stop() }
+    }
+
+    private fun bindAllCameraUseCases() {
+        if (cameraProvider != null) {
+            cameraProvider!!.unbindAll()
+            bindAnalysisUseCase()
+        }
+    }
+
+    private fun bindAnalysisUseCase() {
+        if (cameraProvider == null) {
+            return
+        }
+        if (analysisUseCase != null) {
+            cameraProvider!!.unbind(analysisUseCase)
+        }
+        if (imageProcessor != null) {
+            imageProcessor!!.stop()
+        }
+        imageProcessor = try {
+                val optionsBuilder = FaceDetectorOptions.Builder()
+                        .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+                        .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+                        .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+                        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+                        .setMinFaceSize(0.1f)
+
+                //optionsBuilder.enableTracking()
+                val faceDetectorOptions = optionsBuilder.build()
+                FaceDetectorProcessor(this, faceDetectorOptions)
+            } catch (e: Exception) {
+                Toast.makeText(applicationContext, "Can not create image processor: " + e.localizedMessage, Toast.LENGTH_LONG).show()
+                return
+            }
+
+        analysisUseCase = ImageAnalysis.Builder().build()
+
+        needUpdateOverlayInfo = true
+
+        analysisUseCase?.setAnalyzer(
+            // imageProcessor.processImageProxy will use another thread to run the detection underneath,
+            // thus we can just runs the analyzer itself on main thread.
+            ContextCompat.getMainExecutor(this)) { imageProxy: ImageProxy ->
+            if (needUpdateOverlayInfo) {
+                val isImageFlipped = cameraSelector == CameraSelector.DEFAULT_FRONT_CAMERA
+                val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+                if (rotationDegrees == 0 || rotationDegrees == 180) {
+                    graphicOverlay.setImageSourceInfo(imageProxy.width, imageProxy.height, isImageFlipped)
+                } else {
+                    graphicOverlay.setImageSourceInfo(imageProxy.height, imageProxy.width, isImageFlipped)
+                }
+                needUpdateOverlayInfo = false
+            }
+            try {
+                imageProcessor!!.processImageProxy(imageProxy, graphicOverlay)
+            } catch (e: MlKitException) {
+                Toast.makeText(applicationContext, e.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+        cameraProvider!!.bindToLifecycle(this, cameraSelector, analysisUseCase)
+    }
+}
---
+++ app/src/main/java/io/github/chayanforyou/drowsinessdetection/processors/FaceDetectorProcessor.kt
@@ -0,0 +1,84 @@
+package io.github.chayanforyou.drowsinessdetection.processors
+
+import android.content.Context
+import android.util.Log
+import com.google.android.gms.tasks.Task
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.Face
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetector
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import com.google.mlkit.vision.face.FaceLandmark
+import io.github.chayanforyou.drowsinessdetection.overlay.GraphicOverlay
+import io.github.chayanforyou.drowsinessdetection.FaceGraphic
+import io.github.chayanforyou.drowsinessdetection.utils.SoundPoolManager
+import java.util.Locale
+
+class FaceDetectorProcessor(context: Context, detectorOptions: FaceDetectorOptions?) :
+  VisionProcessorBase<List<Face>>(context) {
+
+  private val detector: FaceDetector
+  private val soundManager: SoundPoolManager
+  private var eyesClosedFrameCount = 0
+
+  init {
+    val options = detectorOptions
+      ?: FaceDetectorOptions.Builder()
+        .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+        .enableTracking()
+        .build()
+
+    detector = FaceDetection.getClient(options)
+    soundManager = SoundPoolManager.getInstance(context)
+
+    Log.v(MANUAL_TESTING_LOG, "Face detector options: $options")
+  }
+
+  override fun stop() {
+    super.stop()
+    detector.close()
+    soundManager.release()
+  }
+
+  override fun detectInImage(image: InputImage): Task<List<Face>> {
+    return detector.process(image)
+  }
+
+  override fun onSuccess(faces: List<Face>, graphicOverlay: GraphicOverlay) {
+    for (face in faces) {
+      val leftEyeOpen = face.leftEyeOpenProbability
+      val rightEyeOpen = face.rightEyeOpenProbability
+
+      if (leftEyeOpen != null && rightEyeOpen != null) {
+        if (leftEyeOpen < EYES_CLOSED_THRESHOLD && rightEyeOpen < EYES_CLOSED_THRESHOLD) {
+          eyesClosedFrameCount++
+
+          if (eyesClosedFrameCount >= framesPerSecond / 2) {
+            eyesClosedCount++
+            eyesClosedFrameCount = 0
+
+            if (eyesClosedCount >= ALARM_COUNT_THRESHOLD) {
+              alarmCount++
+              eyesClosedCount = 0
+              soundManager.playSound()
+            }
+          }
+        } else {
+          eyesClosedFrameCount = 0
+        }
+      }
+
+      graphicOverlay.add(FaceGraphic(graphicOverlay, face))
+    }
+  }
+
+  override fun onFailure(e: Exception) {
+    Log.e(TAG, "Face detection failed $e")
+  }
+
+  companion object {
+    private const val TAG = "FaceDetectorProcessor"
+    private const val EYES_CLOSED_THRESHOLD = 0.50f
+    private const val ALARM_COUNT_THRESHOLD = 10
+  }
+}
